46e09e19b309f5e67af35e984eee56b4b174eaed
Larry Wang <lawrence.anyang.wang@gmail.com>
2020-09-12 11:19:56 -0400

    first check-in

---
+++ fb.py
@@ -0,0 +1,27 @@
+
+
+def print_dict(d):
+    print(d)
+    print(len(d))
+
+tb_dict = {
+    
+}
+
+while True:
+    cmd = input("what do you want to do? ")
+
+    if(cmd=="read"):
+        print_dict(tb_dict)
+
+    if(cmd=="create"):
+        tb_dict[input("name")] = input("phone number?")
+        print_dict(tb_dict)
+
+    if(cmd=="update"):
+        tb_dict[input("porson's name?")] = input("phone number?")
+        print_dict(tb_dict)
+
+    if(cmd=="delete"):
+        tb_dict.pop(input("name?"))
+        print_dict(tb_dict)
