782f2586cf86a9095cc3903e3f19ae71a32a5772
Larry Wang <lawrence.anyang.wang@gmail.com>
2020-09-13 16:20:00 -0400

    number olny

--- fb.py
+++ fb.py
@@ -10,16 +10,27 @@ tb_dict = {
 
 while True:
     cmd = input("what do you want to do? ")
-
     if(cmd=="read"):
         print_dict(tb_dict)
 
     if(cmd=="create"):
-        tb_dict[input("person's name? ")] = input("phone number? ")
+
+        tb_dict[ input("person's name? ")] = pn = input("phone number? ")
+        try:
+            int(pn)
+        except ValueError:
+            print ("number olny.")
+            break
+
         print_dict(tb_dict)
 
     if(cmd=="update"):
-        tb_dict[input("person's name? ")] = input("phone number? ")
+        tb_dict[input("person's name? ")] = npn = input("phone number? ")
+        try:
+            int(npn)
+        except ValueError:
+            print("number olny.")
+            break
         print_dict(tb_dict)
 
     if(cmd=="delete"):
