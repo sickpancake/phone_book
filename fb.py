38d47ffd487b9457d141692fed5f4299a5702210
Larry Wang <lawrence.anyang.wang@gmail.com>
2020-09-14 10:26:02 -0400

    Now the program tell the user what cmd are availble

--- fb.py
+++ fb.py
@@ -9,7 +9,7 @@ tb_dict = {
 }
 
 while True:
-    cmd = input("what do you want to do? ")
+    cmd = input("You can read, create, update and delete. If you don't want to use the code anymore, you can type 'quit' or 'exit'. What do you want to do? ")
     if(cmd=="read"):
         print_dict(tb_dict)
 
