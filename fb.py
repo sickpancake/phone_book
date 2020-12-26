4ef48d653826f67716ed910220ce47df242167db
Larry Wang <lawrence.anyang.wang@gmail.com>
2020-12-26 17:36:32 -0500

    has to be 10 numbers

--- fb.py
+++ fb.py
@@ -15,6 +15,20 @@ while True:
         p = input("person's name? ")
         pn = input("phone number? ")
         tb_dict[(pn)] = p
+        l = len(pn)
+
+        if(l == 10):
+            print("first check done")
+
+        elif(l > 10):
+            print("has to be 10 numbers")
+            break
+
+        else:
+            print("10 numbers")
+            break
+            
+
         try:
             int(pn)
         except ValueError:
@@ -23,12 +37,28 @@ while True:
 
         print_dict(tb_dict)
 
+
+
     
 
     if(cmd=="update"):
         pn2 = input("person's name? ")
         phn2 = input("phone number? ")
         tb_dict[pn2] = npn = phn2
+
+        l2 = len(phn2)
+
+        if(l2 == 10):
+            print("first check done")
+
+        elif(l2 > 11):
+            print("has to be 10 numbers")
+            break
+
+        else:
+            print("10 numbers")
+            break
+
         try:
             int(npn)
         except ValueError:
