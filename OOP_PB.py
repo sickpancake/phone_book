c98b3f7e4b98b457f5cf06a424c72b2260986944
Larry Wang <lawrence.anyang.wang@gmail.com>
2021-08-25 13:01:21 -0400

    deleted all update parts

--- OOP_PB.py
+++ OOP_PB.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
 #main loop
 while True:
-    cmd = input("You can read, create, update and delete. If you don't want to use the code anymore, you can type 'quit' or 'exit'. What do you want to do? ")
+    cmd = input("You can read, create and delete. If you don't want to use the code anymore, you can type 'quit' or 'exit'. What do you want to do? ")
     if(cmd=="read"):
          for conact in phoneBook.list:
             print(conact.name + ': ' + conact.phoneNumber)
@@ -19,22 +19,6 @@ while True:
         
         contact = Contact(p, pn)
         phoneBook.saveContact(contact)
-
-        for conact in phoneBook.list:
-            print(conact.name + ': ' + conact.phoneNumber)
-
-
-    if(cmd=="update"):
-        pn2 = input("person's name? ")
-        order = input("order?")
-        contact = phoneBook.getContactsByNameAndOrder(pn2, order)
-        if (contact == None):
-            print("contact does not exist")
-            continue
-
-        phn2 = input("phone number? ")
-        
-        phoneBook.updateConact(contact, phn2)
 
         for conact in phoneBook.list:
             print(conact.name + ': ' + conact.phoneNumber)
