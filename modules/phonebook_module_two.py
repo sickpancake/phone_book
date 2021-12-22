568fbf53b855eb57f725c411128ff07cfc0d638c
Lawrence Wang <lawrence.anyang.wang@gmail.com>
2021-12-22 12:55:14 -0500

    wip

--- modules/phonebook_module_two.py
+++ modules/phonebook_module_two.py
@@ -10,12 +10,13 @@ from modules.ContactModule import Contact
 
 class PhoneBook:
     '''create phonebook class'''
+
     def __init__(self) -> None:
         # get a consistent path to database across operating systems
         # e.g.
         # Windows: 'C:\\Users\\wangy\\phonebook.db'
         # Linux:   '/home/wxh/phonebook.db'
-        self.dbpath =  os.path.join(pathlib.Path.home(), "phonebook.db")
+        self.dbpath = os.path.join(pathlib.Path.home(), "phonebook.db")
         print('database is saved at: ' + self.dbpath)
 
         self.connection = sqlite3.connect(self.dbpath)
@@ -39,3 +40,26 @@ class PhoneBook:
         )
 
         self.connection.commit()
+
+    def get_contacts(self) -> list[Contact]:
+        """get the contacts of this list"""
+
+        # create an empty contact list first
+        contact_list = list[Contact]()
+
+        # select all contacts from phonebook
+        results = self.cursor.execute('select * from phonebook')
+
+        for each_row in results:
+            # get contact name and phonenumber from this row
+            name = each_row[1]
+            phonenumber = each_row[2]
+
+            # assemble name and phonenumber into a contact
+            contact = Contact(name, phonenumber)
+
+            # add the assembled contact into the list
+            contact_list.append(contact)
+
+        # return the list
+        return contact_list
